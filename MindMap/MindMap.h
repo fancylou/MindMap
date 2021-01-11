9166cb6c7a5b15ce53bf3e81756af77c670e9057
zhongzhiyuan <zhongzhiyuan5@gmail.com>
2021-01-11 14:35:15 +0800

    Initial Commit

---
+++ MindMap/MindMap.h
@@ -0,0 +1,18 @@
+//
+//  MindMap.h
+//  MindMap
+//
+//  Created by 钟志远 on 2021/1/11.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for MindMap.
+FOUNDATION_EXPORT double MindMapVersionNumber;
+
+//! Project version string for MindMap.
+FOUNDATION_EXPORT const unsigned char MindMapVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MindMap/PublicHeader.h>
+
+
